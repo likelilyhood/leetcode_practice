26f756d9fdf4600381594d1ef14fe6f5c968a34b
likelilyhood <91865708@qq.com>
2025-03-16 21:54:10 +0800

    更新TODO.md，添加完成的任务记录；修改字符串反转算法，优化实现并添加示例代码

--- 字符串/reverse2.cpp
+++ 字符串/reverse2.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include<vector>
-
+#include<bits/stdc++.h>
 using namespace std;
 class Soution{
 public:
-    void reverse(vector<char>& s,int k){
-        while(2*k<s.size())
+    string reverseStr(string & s,int k){
+        for (int i=0;i<s.size();i+=2*k){
+            if(i+k<s.size())reverse(s.begin()+i,s.begin()+i+k);
+            else reverse(s.begin()+i,s.end());
+
+        }
+        return s;
     }
 };
+int main(){
+    string s="abcdefg";
+    int k=2;
+    Soution s1;
+    cout<<s1.reverseStr(s,k)<<endl;
+    return 0;
+}
